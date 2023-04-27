b81e1a6eecdda86bcabb344edafcb9a573580d47
Ana Hordieieva <gordeevaisgreat@gmail.com>
2023-04-27 14:45:10 +0300

    added slider

---
+++ script/custom.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".slider").slick({
+    infinite: true,
+    dots: true,
+    prevArrow:
+      '<span class="prev"><img class="arrow" src="./img/icons/prev.svg" /></span>',
+    nextArrow:
+      '<span class="next"><img class="arrow" src="./img/icons/next.svg" /></span>',
+  });
+});

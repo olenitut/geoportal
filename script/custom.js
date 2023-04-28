5cb6ce32150c3f27820f8313ac4306338e4be061
Ana Hordieieva <gordeevaisgreat@gmail.com>
2023-04-28 08:43:03 +0300

    final

--- script/custom.js
+++ script/custom.js
@@ -8,3 +8,26 @@ $(function () {
       '<span class="next"><img class="arrow" src="./img/icons/next.svg" /></span>',
   });
 });
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
